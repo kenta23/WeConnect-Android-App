4c3fb9d743356b1707e88a678952125560cdbf28
JAMSWS <trjnzjames777@gmail.com>
2023-04-24 13:58:45 +0800

    naglagay ako burat sa may line 18

--- app/src/main/java/com/example/weconnect/MainActivity.java
+++ app/src/main/java/com/example/weconnect/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("Hello World");
         System.out.println("This is my code - James");
+
+        System.out.println("Burat");
     }
 }
