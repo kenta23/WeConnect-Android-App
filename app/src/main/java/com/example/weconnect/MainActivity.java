3f7fedbdc2c3371a155f4810d2f15d34bc084d06
Eliputh <117935958+Elipoot@users.noreply.github.com>
2023-04-24 13:52:03 +0800

    added on line 17 on MainActivity

--- app/src/main/java/com/example/weconnect/MainActivity.java
+++ app/src/main/java/com/example/weconnect/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("Hello World");
         System.out.println("Sobrang POGI KO TAENA ");
+        System.out.println("BURAT");
 
     }
 }
