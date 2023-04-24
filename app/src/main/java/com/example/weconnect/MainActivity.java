0a0298da7c2583ee0bd5bf6e0855c86393287dbd
Eliputh <117935958+Elipoot@users.noreply.github.com>
2023-04-24 14:00:15 +0800

    added code on line 16 in MainActivity.java

--- app/src/main/java/com/example/weconnect/MainActivity.java
+++ app/src/main/java/com/example/weconnect/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("Hello World");
         System.out.println("Burat ko Malaki");
+        System.out.println("Burati");
 
     }
 }
