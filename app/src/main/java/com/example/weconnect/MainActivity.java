36ffd597a0e0f0be9df078b0779b2a1b6f61bff3
JAMSWS <trjnzjames777@gmail.com>
2023-04-24 13:59:52 +0800

    nadelete ko yung burat sa line 18

--- app/src/main/java/com/example/weconnect/MainActivity.java
+++ app/src/main/java/com/example/weconnect/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Hello World");
         System.out.println("This is my code - James");
 
-        System.out.println("Burat");
+
     }
 }
