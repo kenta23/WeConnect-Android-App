a177a597f97b92405c55d38d2e0c5b98ba299115
Eliputh <117935958+Elipoot@users.noreply.github.com>
2023-05-05 10:25:14 +0800

    nireset ko

--- app/src/main/java/com/example/weconnect/Register.java
+++ app/src/main/java/com/example/weconnect/Register.java
@@ -3,263 +3,60 @@ package com.example.weconnect;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import java.util.Objects;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.Calendar;
-import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
 
-    private EditText firstName;
-    private EditText lastName;
-    private EditText email;
-    private EditText date;
-    private EditText password;
-    private EditText confirmPass;
+    //TextInput
+    EditText firstName, lastName, emailAdd, birthDate, password, confirmPassword;
 
-    private RadioGroup radioGroupRegisterGender;
+    //RadioGroup
+    RadioGroup radioGroup;
 
-    private RadioButton radioButtonRegisteredGenderSelected;
-    private Button signup;
+    //Button
+    Button btnSignup;
 
-    private FirebaseAuth auth;
-    private CheckBox checkBox;
-    private static final String TAG = "Register";
-
+    FirebaseDatabase userDatabase;
     DatabaseReference reference;
-    FirebaseDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        emailAdd = findViewById(R.id.emailAdd);
+        birthDate = findViewById(R.id.birthDate);
+        password = findViewById(R.id.password);
+        confirmPassword = findViewById(R.id.confirmPassword);
+        btnSignup = findViewById(R.id.btnSignup);
+        radioGroup = findViewById(R.id.radioGroup);
 
-        //editText
-        firstName = findViewById(R.id.editFirstName);
-        lastName = findViewById(R.id.editLastName);
-        email = findViewById(R.id.editTextTextEmailAddress);
-        date = findViewById(R.id.editTextDate);
-        password = findViewById(R.id.editPassword);
-        confirmPass = findViewById(R.id.editConfirmPassword);
+        //Saving Data in Firebase
 
-        //Button
-        signup = findViewById(R.id.btnSignup);
-
-        //RadioButton for Gender
-        radioGroupRegisterGender = findViewById(R.id.radioGroup);
-        radioGroupRegisterGender.clearCheck();
-
-        //Checkbox
-
-        checkBox = findViewById(R.id.chkAgreement);
-
-        auth = FirebaseAuth.getInstance();
-
-        signup.setOnClickListener(new View.OnClickListener() {
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                int selectedGenderId = radioGroupRegisterGender.getCheckedRadioButtonId();
-                radioButtonRegisteredGenderSelected = findViewById(selectedGenderId);
-
-                //obtain the entered data
-
-                String textFirstName = firstName.getText().toString();
-                String textLastName = lastName.getText().toString();
-                String textEmail = email.getText().toString();
-                String textBirthDate = date.getText().toString();
-                String textPassword = password.getText().toString();
-                String textConfirmPassword = confirmPass.getText().toString();
-                String textGender;
 
 
-                // If Field Data is Empty
-                if(TextUtils.isEmpty(textFirstName)){
-                    Toast.makeText( Register.this, "Please Enter your first name", Toast.LENGTH_LONG).show();
-                    firstName.setError("First name is required");
-                    firstName.requestFocus();
-                } else if (TextUtils.isEmpty(textLastName)){
-                    Toast.makeText( Register.this, "Please Enter your last name", Toast.LENGTH_LONG).show();
-                    lastName.setError("Last name is required");
-                    lastName.requestFocus();
 
-                } else if (TextUtils.isEmpty(textEmail)){
-                    Toast.makeText( Register.this, "Please Enter your email address", Toast.LENGTH_LONG).show();
-                    email.setError("email is required");
-                    email.requestFocus();
-                } else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
-                    Toast.makeText( Register.this, "Please Enter re-enter email address", Toast.LENGTH_LONG).show();
-                    email.setError("email is required");
-                    email.requestFocus();
-                } else if(TextUtils.isEmpty(textBirthDate)){
-                    Toast.makeText( Register.this, "Please Enter your birthdate", Toast.LENGTH_LONG).show();
-                    date.setError("birthdate is required");
-                    date.requestFocus();
-                } else if ( radioGroupRegisterGender.getCheckedRadioButtonId() == -1){
-                    Toast.makeText(Register.this, "Please select your gender", Toast.LENGTH_LONG).show();
-                    radioButtonRegisteredGenderSelected.setError("Gender is required");
-                    radioButtonRegisteredGenderSelected.requestFocus();
-                }else if (TextUtils.isEmpty(textPassword)){
-                    Toast.makeText(Register.this, "Please enter your password", Toast.LENGTH_LONG).show();
-                    password.setError("password is required");
-                    password.requestFocus();
-                }else if(textPassword.length() < 8 ) {
-                    Toast.makeText(Register.this, "Password should be 8 characters long", Toast.LENGTH_LONG).show();
-                }else if(TextUtils.isEmpty(textConfirmPassword)){
-                    Toast.makeText(Register.this, "Please confirm your password", Toast.LENGTH_LONG).show();
-                    confirmPass.setError("Password is required");
-                    confirmPass.requestFocus();
-                }else if (!textPassword.equals(textConfirmPassword)){
-                    Toast.makeText(Register.this, "Password doesn't match", Toast.LENGTH_LONG).show();
-                    confirmPass.setError("Password confirmation required");
-                    //clear entered password
-                    password.clearComposingText();
-                    confirmPass.clearComposingText();
-                }else if (!checkBox.isChecked()){
-                    Toast.makeText(Register.this, "Please click the box if you wish to proceed", Toast.LENGTH_LONG).show();
-                    checkBox.setError("Checkbox is required to be able to proceed");
-                    checkBox.requestFocus();
-                }else{
-                  /*
-                    Users users = new Users(textFirstName, textLastName, textEmail, textDate, textGender, textPassword);
-                    db = FirebaseDatabase.getInstance();
-                    reference = db.getReference("Users");
-                    reference.child(textFirstName).setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-
-
-                            firstName.setText("");
-                            lastName.setText("");
-                            email.setText("");
-                            date.setText("");
-                            password.setText("");
-                            Toast.makeText(Register.this,"Successfully Added",Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                   */
-                    textGender = radioButtonRegisteredGenderSelected.getText().toString();
-                    registerUser(textFirstName, textLastName, textEmail, textPassword, textBirthDate, textGender);
-                    Intent intent = new Intent(Register.this, WelcomeUser.class);
-                    intent.putExtra("user", textFirstName);
-                    startActivity(intent);
-                    finish();
-
-
-                   // reference = FirebaseDatabase.getInstance().getReference();
-
-                  /*  reference.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            if(snapshot.child("Users").hasChild(textEmail)) {
-                                Toast.makeText(Register.this, "Email already exists", Toast.LENGTH_SHORT).show();
-                            }
-                            else {
-                                reference.child("Users").child(textFirstName).child("First name").setValue(textFirstName);
-                                reference.child("Users").child(textFirstName).child("Last name").setValue(textLastName);
-                                reference.child("Users").child(textFirstName).child("Email").setValue(textEmail);
-                                reference.child("Users").child(textFirstName).child("Birthdate").setValue(textBirthDate);
-                                reference.child("Users").child(textFirstName).child("Password").setValue(textPassword);
-
-                                Toast.makeText(Register.this, "Account Registered", Toast.LENGTH_SHORT).show();
-
-                                Intent intent = new Intent(Register.this, WelcomeUser.class);
-                                intent.putExtra("name", textFirstName);
-                                startActivity(intent);
-                                finish();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            Toast.makeText(Register.this, "Error", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                   */
-
-
-                }
-
-            }
-        });
-    }
-        //Register User
-    private void registerUser(String firstname, String lastname, String emailAdd, String password, String birthdate, String gender ) {
-
-        auth.createUserWithEmailAndPassword(emailAdd, password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(Register.this, "Account Registered", Toast.LENGTH_SHORT).show();
-
-                    HashMap<String, Object> map = new HashMap<>();
-                    map.put("First name", firstname);
-                    map.put("Last name", lastname);
-                    map.put("email", emailAdd);
-                    map.put("password", password);
-                    map.put("Birthday", birthdate);
-                    map.put("Gender", gender);
-
-                    FirebaseDatabase.getInstance().getReference().child("User").child("Accounts").child(firstname).updateChildren(map); //use in Hashmap
-
-                    //Open WelcomeUser Activity
-                    Intent intent = new Intent(Register.this, WelcomeUser.class);
-                    intent.putExtra("email", emailAdd);
-                    startActivity(intent);
-                    //Prevent user from returning back to Register Activity incase they click back button after Registration
-                    finish();
-
-                }else{
-                    Toast.makeText(Register.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-
-
-                    try{
-                       throw task.getException();
-                    }catch (FirebaseAuthInvalidCredentialsException e){
-                        email.setError("Your email is invalid. use a valid email");
-                        email.requestFocus();
-                    }catch (FirebaseAuthUserCollisionException e){
-                        email.setError("User is already registered with this email. Please use another email.");
-                        email.requestFocus();
-                    }catch(Exception e){
-                        Log.e(TAG, e.getMessage());
-                        Toast.makeText(Register.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-
-
-                }
             }
         });
 
 
     }
 }
+
